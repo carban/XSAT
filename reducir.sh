4f97db724c229a287dca13f558e463bcb73b7ecc
carban <murillo.carlos@correounivalle.edu.co>
2020-10-19 15:14:03 -0500

    correction in names

---
+++ reducir.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for file in InstanciasSAT/*
+do
+    filename=$(echo $file | sed 's/InstanciasSAT//g')
+    python Reductor/newxsat.py $file $1 > X-SAT/$filename
+    echo "Done"
+done
--- s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for file in instances/*
-do
-    filename=$(echo $file | sed 's/instances//g')
-    python newxsat.py $file $1 > reduced/$filename
-    echo "Done"
-done
