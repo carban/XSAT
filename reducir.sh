dc8d0540e55b123d333c563b44b1c893928f05c6
carban <murillo.carlos@correounivalle.edu.co>
2020-10-19 19:17:02 -0500

    correction

--- reducir.sh
+++ reducir.sh
@@ -3,6 +3,6 @@
 for file in InstanciasSAT/*
 do
     filename=$(echo $file | sed 's/InstanciasSAT//g')
-    python Reductor/newxsat.py $file $1 > X-SAT/$filename
+    python Reductor/reducer.py $file $1 > X-SAT/$filename
     echo $filename "Done"
 done
